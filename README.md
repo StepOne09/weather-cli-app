0495a80945d32942d39cd5fd616d2f6e82d32c02
StepOne09 <xmail1357@gmail.com>
2026-05-09 22:17:18 +0600

    Initial Weather CLI App

---
+++ README.md
@@ -0,0 +1,14 @@
+# Weather CLI App
+
+A Python CLI app that shows real-time weather using OpenWeatherMap API.
+
+## Features
+
+- Live weather data
+- Temperature, humidity, condition
+- CLI interface
+
+## Run
+
+```bash
+python app.py
---
+++ app.py
@@ -0,0 +1,32 @@
+import requests
+
+API_KEY = "7a5594bbc82733c062e761f336eccb85"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+def get_weather(city):
+    params = {
+        "q": city,
+        "appid": API_KEY,
+        "units": "metric"
+    }
+
+    response = requests.get(BASE_URL, params=params)
+    data = response.json()
+
+    print(data)  # debug
+
+    cod = str(data.get("cod"))
+
+    if cod != "200":
+        print("\nError:", data.get("message"))
+        return
+
+    print("\n===== WEATHER REPORT =====\n")
+    print("City:", data["name"])
+    print("Temp:", data["main"]["temp"])
+    print("Humidity:", data["main"]["humidity"])
+    print("Condition:", data["weather"][0]["description"])
+
+city = input("Enter city name: ")
+get_weather(city)
+
